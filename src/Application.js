0464bcf62a9cd04c368fe1f80bb5addb62885d78
Steve Kinney <hello@stevekinney.net>
2018-05-14 19:50:18 -0600

    Remove performance stuff

--- src/Application.js
+++ src/Application.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
-import CountDown from './CountDown';
 import NewItem from './NewItem';
 import Items from './Items';
-import PerformanceTools from './PerformanceTools';
 import './Application.css';
-
-let count = 0;
-
-const defaultState = [
-  { value: 'Pants', id: count++, packed: false },
-  { value: 'Jacket', id: count++, packed: false },
-  { value: 'iPhone Charger', id: count++, packed: false },
-  { value: 'MacBook', id: count++, packed: false },
-  { value: 'Sleeping Pills', id: count++, packed: true },
-  { value: 'Underwear', id: count++, packed: false },
-  { value: 'Hat', id: count++, packed: false },
-  { value: 'T-Shirts', id: count++, packed: false },
-  { value: 'Belt', id: count++, packed: false },
-  { value: 'Passport', id: count++, packed: true },
-  { value: 'Sandwich', id: count++, packed: true },
-];
-
-const defaultTripTime = Date.now() + (1000 * 60 * 10);
-const calculateTimeLeft = () => {
-  const distance = defaultTripTime - Date.now();
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  return `in ${minutes} minute(s) and ${seconds} second(s)`;
-};
 
 class Application extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: defaultState,
-      timeLeft: calculateTimeLeft(),
+      items: [],
     };
 
     this.addItem = this.addItem.bind(this);
@@ -43,22 +16,14 @@ class Application extends Component {
     this.markAllAsUnpacked = this.markAllAsUnpacked.bind(this);
   }
 
-  componentDidMount() {
-    this.countDownInterval = setInterval(() => {
-      this.setState({ timeLeft: calculateTimeLeft() });
-    }, 500);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.countDownInterval);
-  }
-
   addItem(item) {
     this.setState({ items: [item, ...this.state.items] });
   }
 
   removeItem(item) {
-    this.setState({ items: this.state.items.filter(other => other.id !== item.id) });
+    this.setState({
+      items: this.state.items.filter(other => other.id !== item.id),
+    });
   }
 
   markAsPacked(item) {
@@ -80,7 +45,6 @@ class Application extends Component {
     return (
       <div className="Application">
         <NewItem onSubmit={this.addItem} />
-        <CountDown {...this.state} />
         <Items
           title="Unpacked Items"
           items={unpackedItems}
@@ -93,13 +57,9 @@ class Application extends Component {
           onCheckOff={this.markAsPacked}
           onRemove={this.removeItem}
         />
-        <button
-          className="button full-width"
-          onClick={this.markAllAsUnpacked}
-        >
+        <button className="button full-width" onClick={this.markAllAsUnpacked}>
           Mark All As Unpacked
         </button>
-        { process.env.NODE_ENV === 'development' && <PerformanceTools /> }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Application />, document.getElementById('root'));
-registerServiceWorker();
